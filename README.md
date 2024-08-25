9a9585ae76002c325ea852c58fe5fe926d51ccff
arpeggito <andresbocarp1@gmail.com>
2024-08-25 12:26:18 +0200

    update

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Correct Registry Script
+# Check and Correct Registry Script
 
 This Python script checks and corrects specific registry keys within the Windows Registry for the Fortinet FortiClient SSL VPN settings. It ensures that the specified registry values are correctly set according to predefined expected values and types. If the values do not match, the script updates them accordingly.
+
+## What's on this repository?
+
+- **check_and_correct_registry.py**: Main script that checks and correct the registry keys within the Windows Registry
+- **check_registry.py**: Individual script that checks the registry keys within the Windows Registry and informs if the values are correct or not.
+- **correct_registry.py**: Individual script that corrects the registry keys within the Windows Registry to the expected values.
+- **run_python_as_ps.ps1**: Run the check_and_correct_registry.py as PowerShell Script
+- **check_correct_registry.ps1**: check_and_correct_registry.py version of the script as PowerShell Script (Since we know that the Scripts and Remediation tool accept only PowerShell Scripts)
+
+The main script for this particular case will be check_and_correct_registry.py since is Python is the programming language I feel comfortable the most with. However, since the task explicitly requires 2 script you'll also find a 2 script version (one that checks and the other one that corrects the registry keys).
 
 ## Understanding the Intune Native Feature
 
@@ -20,11 +30,13 @@ The native feature used to detect and fix issues before users notice is called S
 ## Prerequisites
 
 - **Python 3.x**: Ensure that Python 3.x is installed on your system.
+- **Reg Library**: Understand the methods used by the Reg Library: https://docs.python.org/3/library/winreg.html
 - **Administrator Privileges**: Ideally the script requires administrative privileges to modify the Windows Registry.
+
 
 ## How It Works
 
-### Script Components
+### Script Functions
 
 - **`is_admin()`**: Checks if the script is running with administrative privileges.
 - **`elevate()`**: Re-runs the script with administrative privileges if it is not already elevated.
@@ -49,13 +61,22 @@ If any of these keys are missing or have incorrect values or types, the script w
 
 ## Usage
 
-1. **Run the script**: To use the script, simply run it with Python from your computer:
+1. **Clone the Repository**:
+
+First, clone this repository to your local machine:
+
+    ```bash
+    git clone https://github.com/arpeggito/Microsoft_Azure_Scripts.git
+    cd Microsoft_Azure_Scripts
+    ```
+
+2. **Run the script**: To use the script, simply run it with Python from your computer:
    ```bash
    python check_and_correct_registry.py
 
-2. **Administrator Privileges**: If the script is not run as an administrator, it will request elevation. The script will re-run with the necessary privileges and continue executing.
+3. **Administrator Privileges**: If the script is not run as an administrator, it will request elevation. The script will re-run with the necessary privileges and continue executing.
 
-3. **Check and Correct Registry**: The script will automatically check the specified registry keys and correct any discrepancies.
+4. **Check and Correct Registry**: The script will automatically check the specified registry keys and correct any discrepancies.
 
 ### Error Handling
 
--- check_correct_registry.ps1
+++ check_correct_registry.ps1
@@ -1,17 +1,22 @@
 # Path to the registry key
 $registryPath = "HKLM:\SOFTWARE\Fortinet\FortiClient\Sslvpn\Tunnels\contoso"
 
+# Function to check if the current user is an administrator
 function Test-IsAdmin {
     $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
     return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
+# Function to request elevation if the script is not running as admin
 function Invoke-Elevate {
     if (-not (Test-IsAdmin)) {
         Write-Host "Requesting elevation..."
+        # Prepare arguments for relaunching the script with elevated privileges
         $arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`""
+        # Start a new PowerShell process with elevated privileges
         Start-Process -FilePath "powershell.exe" -ArgumentList $arguments -Verb RunAs -Wait
+        # Exit the current process after starting the elevated one
         Write-Host "Elevation requested. Exiting current process."
         exit
     } else {
@@ -19,19 +24,21 @@ function Invoke-Elevate {
     }
 }
 
+# Function to set a registry value
 function Set-RegistryValue {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$keyName,
+        [string]$keyName, # The name of the registry value to be set
         
         [Parameter(Mandatory=$true)]
-        [string]$valueType,
+        [string]$valueType, # The type of the registry value (e.g., REG_SZ, REG_DWORD)
         
         [Parameter(Mandatory=$true)]
-        $value
+        $value # The value to set for the registry key
     )
 
     try {
+        # Set the registry value based on the specified type
         if ($valueType -eq "REG_SZ") {
             Set-ItemProperty -Path $registryPath -Name $keyName -Value $value
         } elseif ($valueType -eq "REG_DWORD") {
@@ -39,46 +46,57 @@ function Set-RegistryValue {
         }
         Write-Host "$keyName set to $value"
     } catch {
+        # Output an error message if the operation fails
         Write-Host "An error occurred: $_"
     }
 }
 
+# Function to check and correct registry values
 function Check-AndCorrectRegistry {
+    # Check if the registry path exists
     if (-not (Test-Path $registryPath)) {
         Write-Host "Registry path $registryPath not found."
         return
     }
 
+    # Define expected registry values and types
     $expectedValues = @{
         "Server"      = @("REG_SZ", "vpn.contoso.com:443")
         "Sso_enabled" = @("REG_DWORD", 0)
         "DATA1"       = @("REG_SZ", "EncLM 65f1d4bc8b4140b3822ea7")
     }
 
+    # Iterate over each expected registry key and value
     foreach ($keyName in $expectedValues.Keys) {
         $expectedType, $expectedValue = $expectedValues[$keyName]
         try {
+            # Get the current value of the registry key
             $actualValue = (Get-ItemProperty -Path $registryPath -Name $keyName).$keyName
+            # Determine the type of the current registry value
             $actualType = if ($expectedType -eq "REG_SZ") { "REG_SZ" } elseif ($expectedType -eq "REG_DWORD") { "REG_DWORD" }
 
+            # Check if the current value matches the expected value and type
             if ($actualType -eq $expectedType -and $actualValue -eq $expectedValue) {
                 Write-Host "$keyName is correct: $actualValue"
             } else {
                 Write-Host "$keyName is incorrect. Expected Value: $expectedValue, Found: $actualValue, Expected Type: $expectedType, Found: $actualType"
+                # Correct the registry value if it does not match the expected value
                 Set-RegistryValue -keyName $keyName -valueType $expectedType -value $expectedValue
                 Write-Host "Correcting registry type and values..."
             }
         } catch {
+            # Handle the case where the registry key is not found
             Write-Host "$keyName not found in registry."
         }
     }
 }
 
+# Main function to control script execution
 function Main {
-    # Elevate if not running as admin
+    # Elevate the script if not running as admin
     Invoke-Elevate
 
-    # Verify elevation
+    # Verify if the script is running with administrative privileges
     if (-not (Test-IsAdmin)) {
         Write-Host "Elevation failed. Script is not running with administrative privileges."
         exit
